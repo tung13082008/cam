44b48ad53b372ee9661a099166d0808751da47a8
tung13082008 <lumtien1234@gmail.com>
2025-04-05 21:23:01 +0700

    server.js

--- server.js
+++ server.js
@@ -44,3 +44,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 server.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
